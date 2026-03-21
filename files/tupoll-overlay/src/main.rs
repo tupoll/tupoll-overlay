dbaeff20be314430e2dc8e604139b9ded98cb4c0
tupoll <tupollgbt@gmail.com>
2026-03-21 03:19:27 +0000

    Update main.rs

--- files/tupoll-overlay/src/main.rs
+++ files/tupoll-overlay/src/main.rs
@@ -60,11 +60,7 @@ sign-manifests = false
 profile-formats = portage-2
 cache-formats = md5-dict
 masters = gentoo"#),
-       ("profiles/targets/amd64/wayland/make.defaults", r#"# These settings were set by the catalyst build script that automatically
-# built this stage.
-# Please consult /usr/share/portage/config/make.conf.example for a more
-# detailed example.
-#COMMON_FLAGS="-march=core2 -ftree-vectorize -mtune=corei7-avx -pipe"
+       ("profiles/targets/amd64/wayland/make.defaults", r#"#COMMON_FLAGS="-march=core2 -ftree-vectorize -mtune=corei7-avx -pipe"
 CFLAGS="-O2 -pipe -march=native"
 CFLAGS="${COMMON_FLAGS}"
 CXXFLAGS="${COMMON_FLAGS}"
@@ -79,56 +75,58 @@ CHOST="x86_64-pc-linux-gnu"
 # This sets the language of build output to English.
 # Please keep this setting intact when reporting bugs.
 LC_MESSAGES=C.utf8
-USE="aac asm alsa a52 accessibility
+USE="aac asm alsa a52 accessibility 
     -berkdb bluetooth
-     -clang cairo  corefonts -consolekit -cups -crypt -cjk
-     dbus djvu dracut dist-kernel dev-rust
+     cairo  corefonts -consolekit -cups -crypt -cjk
+     dbus djvu dracut dist-kernel dev-rust  
      egl elogind -emacs extra 
-     gtk4 gsm gtk gstreamer -gles -gles1 gles2 gold
+     gtk gsm gtk+  gtk3 gstreamer -gles -gles1 -gles2 gold
      harfbuzz h264
-    jpeg jpeg2k
-    id3tag idn icu -ipv6 -iwd
-    flac -fltk fontconfig faad firmware ffmpeg fish
+    jpeg jpeg2k jit
+    idn icu -ipv6 -iwd
+    firmware fishflac -fltk fontconfig faad ffmpeg fish
     kmod
-    lua_targets_lua5-3 lua_single_target_lua5-4 -lua_single_target_lua5-1 lz4  lua_targets_luajit libzfs
+    lua_targets_lua5-3 lua_single_target_lua5-4 -lua_single_target_lua5-1 lua_targets_luajit libzfs
+    llvm lz4 lsms
     mp3 mp4 mpeg -mmal mpg123
     networkmanager
     opengl openh264 opus
-    -pulseaudio png pango pie -pipewire-alsa -pipewire
+    -pulseaudio -pipewire-alsa -pipewire png pango
     rust 
     ssh
     twolame -trash-panel-plugin
     qt6 -qt5 qml
     introspection
     udisks usb userland_GNU
-    wavpack
-    X xwayland -xinerama -xvfb x264 xml xvid xft x265
-    vulkan  v4l -vaapi  3dnow 
+     wayland wavpack
+    -X xwayland -xinerama -xvfb x264 xml xvid xft x265
+    vulkan  v4l -vaapi  3dnow
     zip zstd -zsh"
 
 FEATURES="-network-sandbox"
 
-PYTHON_TARGETS="python3_12 python3_13 python3_14"
+PYTHON_TARGETS="python3_13 python3_14"
 PYTHON_SINGLE_TARGET="python3_13"
 L10N="ru ru-RU"
-#MAKEOPTS="-j4 -l5"
-MAKEOPTS="-j2 -l3"
-#MAKEOPTS="-j5 -l4"
+MAKEOPTS="-j4 -l5"
+#MAKEOPTS="-j2"
+#MAKEOPTS="-j8 -l9"
 INPUT_DEVICES="libinput"
 #GRUB_PLATFORMS="pc"
 PORTDIR_OVERLAY="/var/db/repos/tupoll-overlay${PORTDIR_OVERLAY}"
 ACCEPT_LICENSE="*"
 #VIDEO_CARDS="radeon radeonsi"
+#VIDEO_CARDS="intel"
 VIDEO_CARDS="intel i915"
 GENTOO_MIRRORS="http://mirror.yandex.ru/gentoo-distfiles/ https://mirrors.evowise.com/gentoo/ https://mirrors.lug.mtu.edu/gentoo/ http://distfiles.gentoo.org"
-      "#), 
-        ("profiles/default/linux/amd64/23.0/desktop/wayland/eapi", r#"8"#),    
-        ("profiles/default/linux/amd64/23.0/desktop/wayland/wayland/parent", r#"gentoo:default/linux/amd64/23.0/desktop
+  "#), 
+        ("profiles/default/linux/amd64/23.0/llvm/wayland/eapi", r#"8"#),    
+        ("profiles/default/linux/amd64/23.0/llvm/wayland/wayland/parent", r#"gentoo:default/linux/amd64/23.0/llvm
 ../../../../../../../targets/amd64/wayland"#),
-        ("profiles/default/linux/amd64/23.0/desktop/wayland/parent", r#"gentoo:default/linux/amd64/23.0/desktop
+        ("profiles/default/linux/amd64/23.0/llvm/wayland/parent", r#"gentoo:default/linux/amd64/23.0/llvm
 ../../../../../../targets/amd64/wayland"#), 
        ("profiles/profiles.desc", r#"#arch		profile_directory				status
-amd64           default/linux/amd64/23.0/desktop/wayland           dev"#), 
+amd64           default/linux/amd64/23.0/llvm/wayland           exp"#), 
        ("metadata/repoman/qa_data.yaml", r#"---
 # This yaml syntax file holds various configuration data for
 # the Quality-Assurance checks performed.
@@ -314,7 +312,104 @@ pulseaudio
 -gnome-keyring
 -fuse
  mmal
--efistub"#), ]; 
+-efistub"#),
+
+     ("profiles/targets/amd64/wayland/package.accept_keywords/custom", r#"gui-libs/gtk ~amd64
+dev-scheme/xdgdirs ~amd64
+gui-apps/pinnacle-lock ~amd64
+gui-apps/pinnacle-screenshot ~amd64
+gui-apps/pinnacle-wallpaper ~amd64
+gui-apps/pinnacle-terminal ~amd64
+gui-libs/gtk4-layer-shell ~amd64
+sys-fs/zfs-kmod **
+sys-fs/zfs **
+net-wireless/hidclient ~amd64
+dev-python/dasbus ~amd64
+media-video/soxbar **
+
+
+x11-themes/adapta-gtk-theme ~amd64
+x11-themes/beautyline-icon-theme ~amd64
+x11-themes/gnome-icon-theme-extras ~arm64
+x11-themes/fluent-icon-theme ~amd64
+
+x11-themes/yasis-icon-theme ~amd64
+
+x11-themes/numix-gtk-theme ~amd64
+x11-themes/numix-icon-theme ~amd64
+app-misc/nwg-look ~amd64
+x11-apps/xcur2png ~amd64 "#),
+     ("profiles/targets/amd64/wayland/package.mask/custom", r#"sys-kernel/gentoo-kermel-bin
+sys-kernel/gentoo-kernel
+gnome-extra/zenity "#),
+     ("profiles/targets/amd64/wayland/package.use/custom", r#"media-libs/freetype -harfbuzz
+dev-python/docutils -python_targets_python3_11
+dev-lang/python -bluetooth
+
+sys-auth/seatd server
+
+www-client/vivaldi -qt6
+sys-fs/fuse suid
+sys-libs/gdbm berkdb
+net-wireless/wpa_supplicant tkip wps wep
+sys-libs/ncurses -gpm
+sys-apps/attr -nls
+sys-libs/libcap -pam
+dev-libs/icu abi_x86_32 
+
+sys-libs/zlib minizip
+net-misc/networkmanager -iwd dhcpcd -resolvconf -ppp 
+dev-python/pillow webp
+net-libs/libtorrent-rasterbar python
+media-libs/freetype harfbuzz
+dev-libs/boost python
+app-crypt/pinentry -qt6
+
+gnome-extra/nm-applet appindicator
+net-wireless/wpa_supplicant -gui 
+
+sys-kernel/installkernel dracut
+
+sys-kernel/gentoo-kernel-bin -initramfs
+sys-kernel/gentoo-kernel savedconfig -initramfs
+net-wireless/bluez deprecated
+ 
+x11-libs/libdrm video_cards_amdgpu
+
+media-sound/sox alsa encode ffmpeg flac id3tag ogg 
+media-libs/libaom -examples
+
+media-video/ffmpeg v4l lcms udev drm threads -vpx
+gnome-extra/zenity -webkit 
+media-libs/mesa -wayland -egl -gles -gles1 -gles2
+gui-libs/gtk aqua
+media-libs/libglvnd X
+sys-fs/zfs python_targets_python3_12
+media-libs/vulkan-loader X wayland
+media-libs/mesa X wayland -egl -gles1 -gles2 -llvm -vaapi -vdpau -zstd -xmlconfig gbm gallium
+x11-libs/gtk+ X
+x11-libs/cairo X aqua
+media-libs/libepoxy X
+gui-libs/gtk X
+app-text/xmlto text
+media-libs/gst-plugins-base X egl
+dev-cpp/cairomm X
+dev-python/pillow tiff
+media-libs/babl lcms
+
+media-libs/gegl lcms
+dev-cpp/gtkmm X
+media-plugins/gst-plugins-meta ogg "#),
+     ("profiles/targets/amd64/wayland/package.use.force/custom", r#"##############media-video/vlc libav
+#x11-libs/gtk+:3 -wayland
+#x11-libs/gtk+ -wayland
+
+media-libs/mesa X wayland -egl -gles1 -gles2 llvm -vaapi -vdpau -zstd -xmlconfig gbm gallium "#),
+     ("profiles/targets/amd64/wayland/package.use.mask", r#"####замаскать полностью :media-video/ffmpeg
+#media-sound/pulseaudio-daemon
+sys-boot/uefi-mkconfig "#),
+    
+ ]; 
      
      for (rel_path, content) in &text_files {
         let full_path = home_base.join(rel_path.trim_start_matches('/'));
